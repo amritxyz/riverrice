92a4ce2bb95a5a0eadaff7fbb9289516c37680c4
amritxyz <amrit44404@proton.me>
2025-04-02 20:20:12 +0545

    width=50^

--- .local/bin/key.sh
+++ .local/bin/key.sh
@@ -1,3 +1,3 @@
-wtype $(grep -v '^#' ~/.local/share/key | fuzzel -T footclient -w 90% -b 181825dd -t cdd6f4ff -m cba6f7ff \
+wtype $(grep -v '^#' ~/.local/share/key | fuzzel -T footclient -w 50% -b 181825dd -t cdd6f4ff -m cba6f7ff \
 	-s cdd6f4ff -B 0 -r 15 -C cdd6f4ff --font Hack --use-bold -I -D no \
 	-l 3 --filter-desktop $@ <&0 --cache=/dev/null --dmenu | cut -d' ' -f1)
