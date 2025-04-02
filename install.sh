d61df103118404d3e34d3bd388ca9fda29b65fcd
amritxyz <amrit44404@proton.me>
2025-04-03 00:52:41 +0545

    auto-install script

---
+++ install.sh
@@ -0,0 +1,72 @@
+#!/bin/sh
+
+echo "Installing Issential packages.."
+
+# Cleanup first
+read -rep ':: Would you like to cleanup Home Dir? [y/N] ' DLT
+if [[ $DLT == "N" || $DLT == "n" || -z $DLT ]]; then
+	echo "Exiting..."
+	exit 1
+else
+	echo "Cleaning..."
+	sudo rm -rf $HOME/.[!.]*
+fi
+
+# Disable Wifi-Power Saver
+read -rep ':: Would you like to disable wifi powersave? [Y/n] ' WIFI
+if [[ $WIFI == "Y" || $WIFI == "y" || -z $WIFI ]]; then
+	LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
+	echo -e "The following has been added to $LOC.\n"
+	echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+	echo -e "\n"
+	echo -e "Restarting NetworkManager service...\n"
+	sudo systemctl restart NetworkManager
+	sleep 3
+fi
+
+# Enable Multi-Lib Packages
+read -rep ':: Would you like to enable "Multilib Packages" [Y/n] ' MULLIB
+if [[ $MULLIB == "Y" || $MULLIB == "y" || -z $MULLIB ]]; then
+	sudo cp /etc/pacman.conf /etc/pacman.conf.backup
+	mline=$(grep -n "\\[multilib\\]" /etc/pacman.conf | cut -d: -f1)
+	rline=$(($mline + 1))
+	sudo sed -i ''$mline's|#\[multilib\]|\[multilib\]|g' /etc/pacman.conf
+	sudo sed -i ''$rline's|#Include = /etc/pacman.d/mirrorlist|Include = /etc/pacman.d/mirrorlist|g' /etc/pacman.conf
+fi
+
+
+read -rep ':: Would you like to install the packages? [Y/n] ' INST
+if [[ $INST == "Y" || $INST == "y" || -z $INST ]]; then
+	sudo pacman -Sy --needed base-devel && \
+		sudo pacman -S --needed tesseract-data-nep river brightnessctl swaybg foot \
+		waybar tmux mpv neovim pulsemixer bleachbit fastfetch unzip ripgrep imv lf \
+		newsboat waylock wtype wl-clipboard fuzzel htop grim slurp man-db zathura zathura-pdf-poppler \
+		vulkan-intel xdg-desktop-portal-gtk adwaita-icon-theme git-lfs wf-recorder \
+		bluez bluez-utils gimp imagemagick wget deluge-gtk fzf curl cmatrix gnu-netcat \
+		ttf-hack ttf-hack-nerd noto-fonts-emoji \
+		rust go jdk-openjdk nodejs npm php
+fi
+
+# Mkdir
+mkdir -p ~/.local/share ~/.config ~/.local/bin ~/.local/git-repos ~/.local/hugo-dir ~/.local/dox ~/.local/vids ~/.local/music ~/.local/audio
+
+# Post Installation
+git clone --depth=1 https://github.com/amritxyz/wall.git/ ~/.local/share/wall
+git clone --depth=1 https://github.com/amritxyz/dev.git/ ~/.local/dev
+git clone --depth=1 https://github.com/amritxyz/kickstart-nvim.git ~/.config/nvim
+
+# Managing Dotfiles
+cp -r ~/riverrice/.local/share/* ~/.local/share
+cp -r ~/riverrice/.local/bin/* ~/.local/bin
+cp -r ~/riverrice/.config/* ~/.config
+cp ~/riverrice/.bashrc ~/.bashrc
+cp ~/riverrice/.bash_profile ~/.bash_profile
+cp ~/riverrice/.inputrc ~/.inputrc
+
+# Cleaning Home Dir
+mv ~/riverrice ~/.local/git-repos/
+
+# Start the bluetooth service
+sudo systemctl enable bluetooth.service
+
+echo "Successfully Installed RiverWm / Riverice."
