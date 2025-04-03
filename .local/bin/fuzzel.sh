b207917c03c32b072601168a3b1c3663d5ca6503
amritxyz <amrit44404@proton.me>
2025-04-03 17:34:42 +0545

    bluetooth with fuzzel and other things

--- .local/bin/fuzzel.sh
+++ .local/bin/fuzzel.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-#
-fuzzel -T footclient -w 64 -b 002b36ff -t 839496ff -m dc322fff \
+fuzzel -T footclient -w 64% -b 002b36ff -t 839496ff -m dc322fff \
 	-s 073642ff -B 2 -r 0 -C 839496ff --font Hack -I -D no \
 	--filter-desktop $@ <&0 --cache=/dev/null
