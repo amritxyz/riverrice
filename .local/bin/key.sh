ff35502ca07490e093a8f867ba6a2ea245530956
amritxyz <amrit44404@proton.me>
2025-04-03 12:55:53 +0545

    just minor changes

--- .local/bin/key.sh
+++ .local/bin/key.sh
@@ -1,4 +1,3 @@
-wtype $(grep -v '^#' ~/.local/share/key |
-	fuzzel -T footclient -w 50% -b 002b36ff -t 839496ff -m dc322fff \
+wtype $(grep -v '^#' ~/.local/share/key | fuzzel -T footclient -w 50% -b 002b36ff -t 839496ff -m dc322fff \
 	-s 073642ff -B 0 -r 15 -C 839496ff --font Hack -I -D no \
 	-l 3 --filter-desktop $@ <&0 --cache=/dev/null --dmenu | cut -d' ' -f1)
