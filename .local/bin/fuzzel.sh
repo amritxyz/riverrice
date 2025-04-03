1bffb43dfb91dfca8ddd773be0167c96c6764cc5
amritxyz <amrit44404@proton.me>
2025-04-03 13:05:18 +0545

    some color schemes and border modified

--- .local/bin/fuzzel.sh
+++ .local/bin/fuzzel.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
-fuzzel -T footclient -w 50% -b 002b36ff -t 839496ff -m dc322fff \
-	-s 073642ff -B 0 -r 15 -C 839496ff --font Hack -I -D no \
+#
+fuzzel -T footclient -w 64 -b 002b36ff -t 839496ff -m dc322fff \
+	-s 073642ff -B 2 -r 0 -C 839496ff --font Hack -I -D no \
 	--filter-desktop $@ <&0 --cache=/dev/null
--- .local/bin/snippet.sh
+++ .local/bin/snippet.sh
@@ -1,3 +1,3 @@
 wtype $(grep -v '^#' ~/.local/share/snippets | fuzzel -T footclient -w 90% -b 002b36ff -t 839496ff -m dc322fff \
-	-s 073642ff -B 0 -r 15 -C 839496ff --font Hack -I -D no \
+	-s 073642ff -B 2 -r 0 -C 839496ff --font Hack -I -D no \
 	--filter-desktop $@ <&0 --cache=/dev/null --dmenu | cut -d' ' -f1)
