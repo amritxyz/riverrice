ccc182f54c429485d085444237f2a309475cd50d
amritxyz <amrit44404@proton.me>
2025-04-13 23:14:42 +0545

    bin: removed desktop.app related things from automated wtype and snippets stuffs

--- .local/bin/key.sh
+++ .local/bin/key.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wtype $(grep -v '^#' ~/.local/share/key | fuzzel -T footclient -w 50% \
+wtype $(grep -v '^#' ~/.local/share/key | fuzzel -w 50% \
 	-b 002b36cc -t 839496ff -m b58900ff -s 073642cc \
 	-B 2 -r 0 -C 839496ff --font Hack -I -D no \
-	-l 3 --filter-desktop "$@" <&0 --cache=/dev/null --dmenu | cut -d' ' -f1)
+	-l 3 --cache=/dev/null --dmenu | cut -d' ' -f1)
--- .local/bin/snippet.sh
+++ .local/bin/snippet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wtype $(grep -v '^#' ~/.local/share/snippets | fuzzel -T footclient -w 90% \
+wtype $(grep -v '^#' ~/.local/share/snippets | fuzzel -w 90% \
 	-b 002b36cc -t 839496ff -m b58900ff \
 	-s 073642cc -B 2 -r 0 -C 839496ff --font Hack -I -D no \
-	--filter-desktop "$@" <&0 --cache=/dev/null --dmenu | cut -d' ' -f1)
+	--cache=/dev/null --dmenu | cut -d' ' -f1)
