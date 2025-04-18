e8078f660d28f38d36b7e2cfbd7e1e7819768cf4
amritxyz <amrit44404@proton.me>
2025-04-18 19:39:05 +0545

    switched to gruvbox_neutral

--- .local/bin/snippet.sh
+++ .local/bin/snippet.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 wtype $(grep -v '^#' ~/.local/share/snippets | fuzzel -w 90% \
-	-b 002b36cc -t 839496ff -m b58900ff \
-	-s 073642cc -B 2 -r 0 -C 839496ff --font Hack -I -D no \
+	-b 282828cc -t 928374ff -m d79921ff \
+	-s 3c3836cc -B 2 -r 0 -C 928374ff --font Hack -I -D no \
 	--cache=/dev/null --dmenu | cut -d' ' -f1)
