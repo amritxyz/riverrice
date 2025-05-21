b22ae5ae3531ea1e101fb01d180b2eba8f625269
amritxyz <amrit44404@proton.me>
2025-05-21 23:13:19 +0545

    changes: just some minor changes

--- scripts/user-js.sh
+++ scripts/user-js.sh
@@ -6,12 +6,12 @@ RICE_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
 # User.js
-NAME="arkenfox.js"
+NAME="$HOME/.local/share/arkenfox.js"
 USER_JS="narsil.js"
 URL="https://raw.githubusercontent.com/amritxyz/user.js/refs/heads/main"
 
 log "Getting "$USER_JS"..."
 
-wget -q --show-progress -O $NAME $URL/$USER_JS
+wget -q --show-progress -O "$NAME" "$URL/$USER_JS"
 
 success "Successfully downloaded user.js."
