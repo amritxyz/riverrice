f6ab59db9e148b116a16c8b1eb4152aaaea588f0
amritxyz <amrit44404@proton.me>
2025-07-29 21:53:34 +0545

    new minimal one

--- scripts/user-js.sh
+++ scripts/user-js.sh
@@ -7,7 +7,7 @@ source "$SCRIPT_DIR/common.sh"
 
 # User.js
 NAME="$HOME/.local/share/arkenfox.js"
-USER_JS="arkenfox.js"
+USER_JS="minimal.js"
 URL="https://raw.githubusercontent.com/amritxyz/user.js/refs/heads/main"
 
 log "Getting "$USER_JS"..."
