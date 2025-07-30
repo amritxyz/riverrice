49f8babb1cdb8dbd1060ed27eeade4c75e72d2d3
amritxyz <amrit44404@proton.me>
2025-07-30 22:35:30 +0545

    feat: added some hardware acceleration stuffs(VA-API)

--- scripts/install.sh
+++ scripts/install.sh
@@ -19,7 +19,7 @@ if [[ $INSTALL == "Y" || $INSTALL == "y" || -z $INSTALL ]]; then
 	echo
 	log "Starting Modular Installation..."
 
-	for group in base fonts daemons deps terminal cli media wm apps dev dev_ext bloat tex; do
+	for group in base intel fonts daemons deps terminal cli media wm apps dev dev_ext bloat tex; do
 		bash "$SCRIPT_DIR/install-group.sh" "$group"
 	done
 
