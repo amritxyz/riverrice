4a93a540fec9031784a3ed100a4c8e239d16f138
amritxyz <amrit44404@proton.me>
2025-08-08 12:32:41 +0545

    chore(scheme): update scheme

--- .local/bin/notes.sh
+++ .local/bin/notes.sh
@@ -11,8 +11,8 @@ TERMINAL="foot"
 newnote() {
     # Use fuzzel to prompt for a new note name
     name=$(fuzzel --dmenu --prompt="Enter note name: " -w 50% \
-                  -b 002b36cc -t 586e75ff -M cb4b16ff -m 657b83ff -S 93a1a1ff \
-                  -s 073642cc -B 2 -r 0 -C 073642ff --input-color cb4b16ff \
+                  -b 1d2021cc -t 766564ff -M e78a4eff -m 928374ff -S ddc7a1ff \
+                  -s 282828cc -B 2 -r 0 -C 3c3836ff --input-color e78a4eff \
                   --font "monospace:size=12" -I -D no -l 0 --cache=/dev/null \
                   <<< "")
 
--- .local/bin/snippet.sh
+++ .local/bin/snippet.sh
@@ -5,6 +5,6 @@
 
 # -M ea6962ff
 wtype $(grep -v '^#' ~/.local/share/snippets | fuzzel -w 90% \
-	-b 002b36cc -t 586e75ff -M cb4b16ff -m 657b83ff -S 93a1a1ff \
-	-s 073642cc -B 2 -r 0 -C 073642ff --input-color cb4b16ff \
+	-b 1d2021cc -t 766564ff -M e78a4eff -m 928374ff -S ddc7a1ff \
+	-s 282828cc -B 2 -r 0 -C 3c3836ff --input-color e78a4eff \
 	--font monospace:size=12 -I -D no --cache=/dev/null --dmenu | cut -d' ' -f1)
