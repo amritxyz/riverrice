20b65c88640f9a2ed4ed71416b3592f6da0779dd
amritxyz <amrit44404@proton.me>
2025-09-05 00:08:59 +0545

    add: just automatically create prac dir

--- scripts/tmp.sh
+++ scripts/tmp.sh
@@ -5,6 +5,7 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/common.sh"
 
 TMP_DIR=/opt/void
+PRAC_DIRS=$TMP_DIR/prac
 
 if [ -d "$TMP_DIR" ]; then
 	warn "$TMP_DIR Exists.."
@@ -12,5 +13,6 @@ else
 	log "Creating tmp env in /opt dir..."
 	sudo mkdir $TMP_DIR
 	sudo chown $USER:$USER $TMP_DIR
-	success "Successfully Created tmp env..."
+	mkdir $PRAC_DIRS/{clang,zig,class,rust,go,java,python,js}
+	success "Successfully Created tmp env with prac dir..."
 fi
