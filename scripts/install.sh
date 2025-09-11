e62a6a7c399a7498aa8eb075edf02de6d5ac4412
amritxyz <amrit44404@proton.me>
2025-09-11 22:53:33 +0545

    new build packages added and shebang typo changed /bin/bash -> /usr/bin/env bash

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,5 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -euo pipefail
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
@@ -19,7 +20,7 @@ if [[ $INSTALL == "Y" || $INSTALL == "y" || -z $INSTALL ]]; then
 	echo
 	log "Starting Modular Installation..."
 
-	for group in base intel fonts daemons deps terminal cli media wm apps dev dev_ext bloat tex; do
+	for group in base intel fonts daemons deps terminal cli media wm apps dev build dev_ext bloat tex; do
 		bash "$SCRIPT_DIR/install-group.sh" "$group"
 	done
 
