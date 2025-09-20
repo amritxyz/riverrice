99a5b353e65eeb1dda117c0ab169b10eacf85751
amritxyz <amrit44404@proton.me>
2025-09-20 18:48:52 +0545

    chore: gruv

--- .local/bin/blue.sh
+++ .local/bin/blue.sh
@@ -6,8 +6,8 @@ get_device_list() {
 
 device_list=$(get_device_list)
 selected_device=$(echo "$device_list" | fuzzel -w 30% \
-	-b 002b36cc -t 586e75ff -M cb4b16ff -m 657b83ff -S 93a1a1ff \
-	-s 073642cc -B 2 -r 0 -C 073642ff --input-color cb4b16ff \
+	-b 1d2021cc -t 766564ff -M e78a4eff -m 928374ff -S ddc7a1ff \
+	-s 282828cc -B 2 -r 0 -C 3c3836ff --input-color e78a4eff \
 	--font monospace:size=12 -I -D no --cache=/dev/null --dmenu --lines 2 | awk '{print $1}')
 
 [ -z "$selected_device" ] && notify-send "No device selected." && exit 1
--- .local/bin/key.sh
+++ .local/bin/key.sh
@@ -3,8 +3,8 @@
 wl-present freeze
 
 wtype $(grep -v '^#' ~/.local/share/key | fuzzel -w 50% \
-	-b 002b36cc -t 586e75ff -M cb4b16ff -m 657b83ff -S 93a1a1ff \
-	-s 073642cc -B 2 -r 0 -C 073642ff --input-color cb4b16ff \
+	-b 1d2021cc -t 766564ff -M e78a4eff -m 928374ff -S ddc7a1ff \
+	-s 282828cc -B 2 -r 0 -C 3c3836ff --input-color e78a4eff \
 	--font monospace:size=12 -I -D no -l 0 --cache=/dev/null --dmenu | cut -d' ' -f1)
 
 wl-present unfreeze
