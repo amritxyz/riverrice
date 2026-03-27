a958c621e1457bd6c2bbfcc30684e270893f2a7a
amritxyz <amrit44404@proton.me>
2026-03-27 12:44:10 +0545

    Merge ralib script with post_install script

--- scripts/post_install.sh
+++ scripts/post_install.sh
@@ -179,3 +179,44 @@ if [ "$LO" = "Y" ] || [ "$LO" = "y" ] || [ -z "$LO" ]; then
 	sudo pacman -S libreoffice-fresh
 	success "Libre-office successfully installed."
 fi
+
+
+ask ":: Would you like to download Raylib for Wayland? [Y/n] "
+read -r LO
+if [ "$LO" = "Y" ] || [ "$LO" = "y" ] || [ -z "$LO" ]; then
+	TMP_DIR="/opt/void"
+	LIBS_DIR="$TMP_DIR/libs"
+
+	if [ -d "$TMP_DIR" ]; then
+		warn "$TMP_DIR Exists..."
+	else
+		log "Creating tmp env in /opt dir..."
+		sudo mkdir -p $TMP_DIR
+		sudo chown $USER:$USER $TMP_DIR
+		success "Successfully Created tmp dir..."
+	fi
+
+	if [ ! -d "$LIBS_DIR" ]; then
+		mkdir -p $LIBS_DIR
+	else
+		warn "$LIBS_DIR Exists..."
+	fi
+
+	log "Downloading Raylib..."
+
+	if [ ! -d "$LIBS_DIR/raylib" ]; then
+		git clone --depth=1 https://github.com/amritxyz/raylib.git "$LIBS_DIR/raylib"
+		cd "$LIBS_DIR/raylib/src"
+		make clean -j12
+		make -j12 GLFW_LINUX_ENABLE_WAYLAND=TRUE
+		sudo make install
+		success "Successfully Downloaded Raylib."
+	elif [ -d "$LIBS_DIR/raylib/src" ]; then
+		cd "$LIBS_DIR/raylib/src"
+		make clean -j12
+		make -j12 GLFW_LINUX_ENABLE_WAYLAND=TRUE
+		sudo make install
+	else
+		warn "Unknown error, Skipping."
+	fi
+fi
